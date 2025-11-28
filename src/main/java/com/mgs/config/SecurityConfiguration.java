9198bafa549bb5fc071543c941130844c44f9c72
Satish Kalyankar <satish.kalyankar@multigenesys.com>
2025-11-28 13:19:42 +0530

    Add REST endpoint to retrieve all application enumerations

--- src/main/java/com/mgs/config/SecurityConfiguration.java
+++ src/main/java/com/mgs/config/SecurityConfiguration.java
@@ -36,6 +36,7 @@ public class SecurityConfiguration {
                 authz
                     .requestMatchers(mvc.pattern(HttpMethod.POST, "/api/authenticate")).permitAll()
                     .requestMatchers(mvc.pattern(HttpMethod.GET, "/api/authenticate")).permitAll()
+                    .requestMatchers(mvc.pattern(HttpMethod.GET, "/api/enums")).permitAll()
                     .requestMatchers(mvc.pattern("/api/admin/**")).hasAuthority(AuthoritiesConstants.ADMIN)
                     .requestMatchers(mvc.pattern("/api/**")).authenticated()
                     .requestMatchers(mvc.pattern("/v3/api-docs/**")).hasAuthority(AuthoritiesConstants.ADMIN)
